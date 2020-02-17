dfd3bd8f85e666f504016099f589f8164cfe6c4f
Shradha <shradha.sgl@gmail.com>
2020-02-17 17:30:15 +0530

    Making components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route, Link} from "react-router-dom";
+import { BrowserRouter as Router, Route} from "react-router-dom";
 
 import Navbar from "./components/navbar.component"
+import CustomerNavbar from "./components/customer-navbar.component"
+import VendorNavbar from "./components/vendor-navbar.component"
 import Register from "./components/register.component";
 import Login from "./components/login.component";
-import Search from "./components/search.component";
+import Search from "./components/product-search.component";
 import Product from "./components/product.component";
 import Vendor from "./components/vendor-product.component";
 import Orders from "./components/order.component";
@@ -13,24 +15,40 @@ import Front from "./components/front.component";
 
 
 
-function App() {
-  return (
-    <Router>
-      <div className="container">
-      <Navbar />
-      <br/>
-      
-      <Route path="/" component={Front} />
-      <Route path="/register" component={Register} />
-      <Route path="/login" exact component={Login} />
-      <Route path="/search" exact component={Search} />
-      <Route path="/products" exact component={Product} />
-      <Route path="/vendor" exact component={Vendor} />
-      <Route path="/orders" exact component={Orders} />
-      </div>
+class App extends React.Component {
+  render() {
+    let user_type = localStorage.getItem('user_type');  
+    let navbar = null;
+    if(user_type === "V")
+      navbar = <VendorNavbar />;
+    else if(user_type === "C")
+      navbar = <CustomerNavbar />;
+    else
+      navbar = <Navbar />;
 
-    </Router>
-  );
+    
+    return (
+      <Router>
+        <div className="container">
+          {navbar}
+          <br></br>
+          <Route exact path="/" render={()=> {
+            if(user_type === "V") return <Orders/>
+            else if(user_type === "C") return <Orders/>
+            else return <Front/>
+          }}  />
+          <Route path="/register" component={Register} />
+          <Route path="/login" component={Login} />
+          <Route path="/search" component={Search} />
+          <Route path="/products" component={Product} />
+          <Route path="/vendor" component={Vendor} />
+          <Route path="/orders" component={Orders} />
+          <Route path="/dashboard" component={Orders} />
+        </div>
+
+      </Router>
+    );
+  }
 }
 
 export default App;
